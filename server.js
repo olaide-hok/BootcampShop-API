5ce861eec9f0df5991145bb92a243b681659e3aa
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-15 00:12:19 +0100

    Create routes and responses in Express

--- server.js
+++ server.js
@@ -7,6 +7,32 @@ dotenv.config({path: './config/config.env'})
 // Initialize App
 const app = express()
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Show all bootcamps'})
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Get bootcamp ${req.params.id}`})
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Create new bootcamp'})
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Update bootcamp ${req.params.id}`,
+    })
+})
+
+app.delete('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Delete bootcamp ${req.params.id}`,
+    })
+})
+
 // Port to listen on
 const PORT = process.env.PORT || 5000
 
