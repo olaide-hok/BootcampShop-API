6bc33dd94a908766345d99b7fd58f3bf048633b5
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-15 16:14:05 +0100

    Connect server file to DB.
    Add promise rejection handlings for unhandled server errors.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 //  Load env vars
 dotenv.config({path: './config/config.env'})
+
+// Connect to database
+connectDB()
 
 // Initialize App
 const app = express()
@@ -22,9 +26,16 @@ app.use('/api/v1/bootcamps', bootcamps)
 // Port to listen on
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
     )
 )
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    // Close server connection and exit process
+    server.close(() => process.exit(1))
+})
