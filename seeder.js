2c2268d4aa9436a893478ee9643bb1fa2e9926d2
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-17 17:43:16 +0100

    Setup Course seeding and add mongoose.connect options to increase timeouts

--- seeder.js
+++ seeder.js
@@ -8,19 +8,27 @@ dotenv.config({path: './config/config.env'})
 
 // Load models
 const Bootcamp = require('./models/Bootcamps')
+const Course = require('./models/Course')
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 10000, // Timeout for server selection
+    socketTimeoutMS: 45000, // Timeout for operations
+})
 
 // Read JSON files
 const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+)
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 )
 
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
+        await Course.create(courses)
         console.log(`Data successfully imported...`.green.inverse)
         process.exit()
     } catch (error) {
@@ -32,6 +40,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
+        await Course.deleteMany()
         console.log(`Data successfully destroyed...`.red.inverse)
         process.exit()
     } catch (error) {
