7eb8d1e96f8bd0976ca8d6c9b5919928d1dd447f
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-17 09:31:06 +0100

    Setup Mongoose Error Handling for duplicate key and required/validation fields

--- middleware/error.js
+++ middleware/error.js
@@ -6,10 +6,22 @@ const errorHandler = (err, req, res, next) => {
     // Log to console for dev
     console.log(err.stack.red)
 
-    // Mongoose bad ObjectId
+    // Mongoose bad ObjectId error handling
     if (err.name === 'CastError') {
         const message = `Bootcamp not found with id of ${err.value}`
         error = new ErrorResponse(message, 404)
+    }
+
+    // Mongoose duplicate key error handling
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered'
+        error = new ErrorResponse(message, 400)
+    }
+
+    // Mongoose Validation Error error handling
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map((val) => val.message)
+        error = new ErrorResponse(message, 400)
     }
 
     res.status(error.statusCode || 500).json({
