45e9b0ac0682f8ceea1ebf465fd246c7f8ceb813
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-17 11:03:15 +0100

    Create seeder file to import and delete bootcamp data

---
+++ seeder.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+const colors = require('colors')
+const dotenv = require('dotenv')
+
+// Load env vars
+dotenv.config({path: './config/config.env'})
+
+// Load models
+const Bootcamp = require('./models/Bootcamps')
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+
+// Read JSON files
+const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+)
+
+// Import into DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps)
+        console.log(`Data successfully imported...`.green.inverse)
+        process.exit()
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+// Delete Data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany()
+        console.log(`Data successfully destroyed...`.red.inverse)
+        process.exit()
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+if (process.argv[2] === '-i') {
+    importData()
+} else if (process.argv[2] === '-d') {
+    deleteData()
+}
