6454c424126286bc1fef895f055358139f486615
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-18 07:37:06 +0100

    Populate bootcamp controller with courses details.
    Update DEL method to delete bootcamp courses.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -28,7 +28,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     )
 
     // Finding resources
-    query = Bootcamp.find(JSON.parse(queryStr))
+    query = Bootcamp.find(JSON.parse(queryStr)).populate('courses')
 
     // Select Fields
     if (req.query.select) {
@@ -131,7 +131,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE//api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+    const bootcamp = await Bootcamp.findById(req.params.id)
 
     if (!bootcamp) {
         return next(
@@ -141,6 +141,9 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
             )
         )
     }
+
+    await bootcamp.deleteOne()
+    // await bootcamp.remove()
 
     res.status(200).json({success: true, data: {}})
 })
