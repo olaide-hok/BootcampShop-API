856d559530e8aa89cd6995bfe54b037592f228d2
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-21 17:28:53 +0100

    Setup Course ownership for PUT, DELETE and POST controllers endpoint.

--- controllers/courses.js
+++ controllers/courses.js
@@ -46,6 +46,8 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
     req.body.bootcamp = req.params.bootcampId
+    req.body.user = req.user.id
+
     const bootcamp = await Bootcamp.findById(req.params.bootcampId)
 
     if (!bootcamp) {
@@ -54,6 +56,16 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
                 `No bootcamp with id of ${req.params.bootcampId}`
             ),
             404
+        )
+    }
+
+    // Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(
+                `User with id of ${req.user.id} is not authorized to add a course to this bootcamp with id ${bootcamp._id}.`,
+                401
+            )
         )
     }
 
@@ -78,6 +90,16 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
         )
     }
 
+    // Make sure user is course owner
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(
+                `User with id of ${req.user.id} is not authorized to update course with id ${course._id}.`,
+                401
+            )
+        )
+    }
+
     course = await Course.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true,
@@ -99,6 +121,16 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
         return next(
             new ErrorResponse(`No course with id of ${req.params.id}`),
             404
+        )
+    }
+
+    // Make sure user is course owner
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(
+                `User with id of ${req.user.id} is not authorized to delete course with id ${course._id}.`,
+                401
+            )
         )
     }
 
