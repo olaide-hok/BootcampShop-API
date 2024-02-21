f44b1723a5fba20eda8a2b35f9bcc063c74b182d
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-21 15:04:16 +0100

    Add authorize middleware to PUT, DELETE and POST courses routes

--- routes/courses.js
+++ routes/courses.js
@@ -8,7 +8,7 @@ const {
 } = require('../controllers/courses')
 
 // Protect auth middleware
-const {protect} = require('../middleware/auth')
+const {protect, authorize} = require('../middleware/auth')
 
 const Course = require('../models/Course')
 const advancedResults = require('../middleware/advancedResults')
@@ -24,11 +24,11 @@ router
         }),
         getCourses
     )
-    .post(protect, addCourse)
+    .post(protect, authorize('publisher', 'admin'), addCourse)
 router
     .route('/:id')
     .get(getCourse)
-    .put(protect, updateCourse)
-    .delete(protect, deleteCourse)
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteCourse)
 
 module.exports = router
