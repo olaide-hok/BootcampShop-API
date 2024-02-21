2cf2a5c8c08216c28feed774e32de3bb146660d5
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-21 08:46:10 +0100

    Add getMe route to auth routes which uses the auth protect middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express')
-const {register, login} = require('../controllers/auth')
+const {register, login, getMe} = require('../controllers/auth')
 
 const router = express.Router()
 
+// Protect auth middleware
+const {protect} = require('../middleware/auth')
+
 router.post('/register', register)
 router.post('/login', login)
+// router.get('/me', protect, getMe) // same as the line below
+router.route('/me').get(protect, getMe)
 
 module.exports = router
