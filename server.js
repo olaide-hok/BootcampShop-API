f1a6d5931ef43324f81ca3565a86a7685810c0f3
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-23 09:43:10 +0100

    Mount users routes

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(cookieParser())
 const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
 const auth = require('./routes/auth')
+const users = require('./routes/users')
 
 // Dev logging middleware (morgan)
 if (process.env.NODE_ENV === 'development') {
@@ -43,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
 app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
 
 // Express Error handler middlewware
 app.use(errorHandler)
