cb541244fd6e189bf2e06c87cdb8a748e6b5fd10
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-24 18:41:51 +0100

    Mount reviews routes

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
 const auth = require('./routes/auth')
 const users = require('./routes/users')
+const reviews = require('./routes/reviews')
 
 // Dev logging middleware (morgan)
 if (process.env.NODE_ENV === 'development') {
@@ -45,6 +46,7 @@ app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
 app.use('/api/v1/auth', auth)
 app.use('/api/v1/users', users)
+app.use('/api/v1/reviews', reviews)
 
 // Express Error handler middlewware
 app.use(errorHandler)
