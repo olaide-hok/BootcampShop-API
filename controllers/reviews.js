a186ee82ee6c975aac27986398a923657bf067ae
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-24 23:18:29 +0100

    Create update and delete reviews route controllers

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -70,3 +70,70 @@ exports.addReview = asyncHandler(async (req, res, next) => {
         data: review,
     })
 })
+
+// @desc    Update review
+// @route   PUT /api/v1/reviews/:id
+// @access  Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+    let review = await Review.findById(req.params.id)
+
+    if (!review) {
+        return next(
+            new ErrorResponse(
+                `No review with id of ${req.params.bootcampId}`,
+                404
+            )
+        )
+    }
+
+    // Make sure review belongs to user or user is admin
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(
+                `User with id of ${req.user.id} is not authorized to update this review with id ${review._id}.`,
+                401
+            )
+        )
+    }
+
+    review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    })
+
+    res.status(200).json({
+        success: true,
+        data: review,
+    })
+})
+
+// @desc    Delete review
+// @route   DELETE /api/v1/reviews/:id
+// @access  Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+    const review = await Review.findById(req.params.id)
+
+    if (!review) {
+        return next(
+            new ErrorResponse(`No review with id of ${req.params.id}`),
+            404
+        )
+    }
+
+    // Make sure user is course owner
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(
+                `User with id of ${req.user.id} is not authorized to delete this review with id ${review._id}.`,
+                401
+            )
+        )
+    }
+
+    await review.deleteOne()
+
+    res.status(200).json({
+        success: true,
+        data: {},
+    })
+})
