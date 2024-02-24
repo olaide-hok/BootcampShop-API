db9c9fd1b19fa3490dcc4ecc5b636b6b7e462afb
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-24 20:36:53 +0100

    Add reviews data file.
    Increase serverSelectionTimeoutMS

--- seeder.js
+++ seeder.js
@@ -10,10 +10,11 @@ dotenv.config({path: './config/config.env'})
 const Bootcamp = require('./models/Bootcamp')
 const Course = require('./models/Course')
 const User = require('./models/User')
+const Review = require('./models/Review')
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 10000, // Timeout for server selection
+    serverSelectionTimeoutMS: 20000, // Timeout for server selection
     socketTimeoutMS: 45000, // Timeout for operations
 })
 
@@ -27,6 +28,9 @@ const courses = JSON.parse(
 const users = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 )
+const reviews = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+)
 
 // Import into DB
 const importData = async () => {
@@ -34,6 +38,7 @@ const importData = async () => {
         await Bootcamp.create(bootcamps)
         await Course.create(courses)
         await User.create(users)
+        await Review.create(reviews)
         console.log(`Data successfully imported...`.green.inverse)
         process.exit()
     } catch (error) {
@@ -47,6 +52,7 @@ const deleteData = async () => {
         await Bootcamp.deleteMany()
         await Course.deleteMany()
         await User.deleteMany()
+        await Review.deleteMany()
         console.log(`Data successfully destroyed...`.red.inverse)
         process.exit()
     } catch (error) {
