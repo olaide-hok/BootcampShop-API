09f6b9f77021fcf96439f4541923de5494e99a7b
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-02-25 06:30:41 +0100

    Change Mongoose bad ObjectId error message

--- middleware/error.js
+++ middleware/error.js
@@ -8,7 +8,7 @@ const errorHandler = (err, req, res, next) => {
 
     // Mongoose bad ObjectId error handling
     if (err.name === 'CastError') {
-        const message = `Resource not found with id of ${err.value}`
+        const message = `Resource not found.`
         error = new ErrorResponse(message, 404)
     }
 
